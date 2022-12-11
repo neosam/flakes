2272ac0a676eb7e9e89b20667601d3e0c8595445
Simon Goller <simon.goller+github@posteo.de>
2022-12-11 08:59:53 +0100

    Update

--- hosts/home.nix
+++ hosts/home.nix
@@ -5,7 +5,14 @@
   home.homeDirectory = "/home/neosam";
   programs.home-manager.enable = true;
 
-  home.packages = with pkgs; [ htop gitui helix git-town firefox ];
+  home.packages = with pkgs; [ 
+    htop 
+    gitui 
+    helix 
+    git-town 
+    firefox
+    vscode
+  ];
 
   home.stateVersion = "22.11";
 }
