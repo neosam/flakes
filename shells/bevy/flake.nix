6a8f92188471ef725d924642a16ec715631b7872
Simon Goller <neosam+github@posteo.de>
2023-01-02 21:01:28 +0100

    Adjust bevy-shell to support macos

--- shells/bevy/flake.nix
+++ shells/bevy/flake.nix
@@ -8,18 +8,25 @@
     flake-utils.lib.eachDefaultSystem
     (system:
       let pkgs = nixpkgs.legacyPackages.${system};
-          buildInputs = with pkgs; [
-            cargo rustc rust-analyzer
+          rustInputs = with pkgs; [cargo rustc rust-analyzer];
+          linuxInputs = with pkgs; [
             udev alsa-lib vulkan-loader
             xlibsWrapper xorg.libXcursor xorg.libXrandr xorg.libXi # To use x11 feature
             libxkbcommon wayland # To use wayland feature
           ];
+          # From here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/darwin/apple-sdk/frameworks.nix
+          darwinInputs = with pkgs.darwin.apple_sdk.frameworks; [
+            ApplicationServices CoreGraphics CoreVideo CoreFoundation
+            AppKit QuartzCore Foundation CoreGraphics Metal CoreServices
+          ];
+          nativeInputs = with pkgs; if pkgs.stdenv.isDarwin then darwinInputs else linuxInputs;
+          buildInputs = rustInputs ++ nativeInputs;
       in
       {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             pkg-config
-            llvmPackages.bintools # To use lld linker
+            #llvmPackages.bintools # To use lld linker
             python3
           ];
           inherit buildInputs;
